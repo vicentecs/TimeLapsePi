bdf4343ca2fa4eb9ea11c637ad6827a28e0da795
Vicente Carletto Scalfoni <vicente@scalfoni.com.br>
2025-04-13 00:02:04 -0300

    add parameter dia

--- _start.sh
+++ _start.sh
@@ -7,7 +7,7 @@ IFS=$'\n\t'
 
 export WORK_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$WORK_PATH/.env"
-export DIA="$(date +"%Y-%m-%d")"
+export DIA="${2:-$(date +"%Y-%m-%d")}"
 export IMG_PATH="${WORK_PATH}/img_${DIA}"
 export NOME_VIDEO="Replay_${DIA}.mp4"
 export VIDEO_PATH="${WORK_PATH}/${NOME_VIDEO}"
@@ -15,7 +15,7 @@ export AUDIO_PATH="${WORK_PATH}/audio.mp3"
 export LOGO_PATH="${WORK_PATH}/logo.png"
 
 function usage() {
-  echo "Uso: $0 [baixar|validar|gerar|subir|tudo]"
+  echo "Uso: $0 [baixar|validar|gerar|subir|tudo] [DIA opcional no formato AAAA-MM-DD] "
   exit 1
 }
 
